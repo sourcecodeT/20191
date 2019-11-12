9f84f7ae029e0f0b838c72f0e39183ae822719e2
thanhdn <daongocthanh98hy@gmail.com>
2019-11-12 16:07:50 +0700

    update

--- compiler-construction-lab/semantic2/src/main.c
+++ compiler-construction-lab/semantic2/src/main.c
@@ -1,9 +1,3 @@
-/* 
- * @copyright (c) 2008, Hedspi, Hanoi University of Technology
- * @author Huu-Duc Nguyen
- * @version 1.0
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,17 +6,19 @@
 
 /******************************************************************/
 
-int main(int argc, char *argv[]) {
-  // if (argc <= 1) {
-  //   printf("parser: no input file.\n");
-  //   return -1;
-  // }
+int main(int argc, char *argv[])
+{
+  if (argc <= 1)
+  {
+    printf("parser: no input file.\n");
+    return -1;
+  }
 
-
-  if (compile("../tests/example3.kpl") == IO_ERROR) {
+  if (compile(argv[1]) == IO_ERROR)
+  {
     printf("Can\'t read input file!\n");
     return -1;
   }
-    
+
   return 0;
 }
