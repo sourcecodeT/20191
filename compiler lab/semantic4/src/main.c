7a2e13ebb8bfc44fa6bc66a4015ac8b3d0ea4c57
thanhdn <daongocthanh98hy@gmail.com>
2020-01-05 21:15:57 +0700

    update

--- compiler lab/semantic4/src/main.c
+++ compiler lab/semantic4/src/main.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * @copyright (c) 2008, Hedspi, Hanoi University of Technology
  * @author Huu-Duc Nguyen
  * @version 1.0
@@ -7,23 +7,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "reader.h"
 #include "parser.h"
+#include "reader.h"
 
 /******************************************************************/
 
-int main(int argc, char *argv[])
-{
-  // if (argc <= 1) {
-  //   printf("parser: no input file.\n");
-  //   return -1;
-  // }
+int main(int argc, char *argv[]) {
+  if (argc <= 1) {
+    printf("parser: no input file.\n");
+    return -1;
+  }
 
-  // if (compile(argv[1]) == IO_ERROR)
-  // {
-  //   printf("Can\'t read input file!\n");
-  //   return -1;
-  // }
-  compile("../tests/example4.kpl");
+  if (compile(argv[1]) == IO_ERROR) {
+    printf("Can\'t read input file!\n");
+    return -1;
+  }
   return 0;
 }
